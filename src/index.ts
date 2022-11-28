d820910e453ec224a2e1c1530f274583a9d2be89
InkoHX <me@inkohx.dev>
2022-11-29 00:32:48 +0900

    fix: remove `oauth1a` function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import { OAuth1a, OAuth1aCredential } from './oauth1a.js'
-
 export type { OAuth1aCredential } from './oauth1a.js'
 export { OAuth1a } from './oauth1a.js'
-
-export const oauth1a = (credential: OAuth1aCredential): OAuth1a =>
-  new OAuth1a(credential)
