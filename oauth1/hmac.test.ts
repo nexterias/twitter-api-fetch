4b43f01e1c962e1f2276b9721bec1b416ff147ae
InkoHX <me@inkohx.dev>
2023-01-21 17:07:46 +0900

    test: add sign test case

--- oauth1/hmac.test.ts
+++ oauth1/hmac.test.ts
@@ -1,5 +1,6 @@
 import * as hmac from "./hmac.ts";
 import { assertStrictEquals } from "../deps_test.ts";
+import * as oauth1a from "./oauth1a.ts";
 
 const credentials = {
   secretAccessToken: "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
@@ -21,4 +22,32 @@ Deno.test("createSigningKey", async () => {
   assertStrictEquals(key.algorithm.name, "HMAC");
   assertStrictEquals(key.type, "secret");
   assertStrictEquals(key.extractable, false);
+});
+
+Deno.test("sign", async () => {
+  const crypto = await hmac.createSigningKey(credentials);
+  const params = oauth1a.createBaseParams(
+    "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+    "xvz1evFS4wEEPTGEFPHBog",
+  );
+
+  params.set("oauth_nonce", "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg");
+  params.set("oauth_timestamp", "1318622958");
+
+  const signedParams = await hmac.sign(
+    crypto,
+    new Request(
+      "https://api.twitter.com/1.1/statuses/update.json?include_entities=true&status=Hello%20Ladies%20%2b%20Gentlemen%2c%20a%20signed%20OAuth%20request%21",
+      {
+        method: "POST",
+      },
+    ),
+    params,
+  );
+
+  assertStrictEquals(
+    signedParams.get("oauth_signature"),
+    "hCtSmYh+iHYCEqBWrE7C7hYmtUk=",
+  );
+  assertStrictEquals(signedParams.get("oauth_signature_method"), "HMAC-SHA1");
 });
